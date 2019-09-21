03cbef75c70cd921fe97cbe6b7a3af4148b19b40
mohamed adel <adel@buseet.com>
2019-09-21 11:17:09 +0200

    Creating Basic Files And Folders

---
+++ App/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dola
+ * Date: 21-Sep-19
+ * Time: 11:11 AM
+ */
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dola
+ * Date: 21-Sep-19
+ * Time: 11:13 AM
+ */
